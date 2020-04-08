774318aee6d36abf15b07fef1678b5b0a1aaa18c
Wan-Nuo, Chiu <sunnusbi@gmail.com>
2020-04-08 16:47:16 +0100

    Initial Commit

---
+++ COVID19UK(API)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  COVID19UK(API)
+//
+//  Created by ChiuWanNuo on 08/04/2020.
+//  Copyright © 2020 ChiuWanNuo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
